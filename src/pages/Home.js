1d194c447c335dc872b1a88f3805c95e7786be96
German Koch <gjkoch7@gmail.com>
2022-03-05 14:59:20 -0300

    Se agrego texto

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import imagen from '../images/imagen.jpg';
 const Home = () => {
     return(
         <div className="home">
+            <h1>vamos boquita</h1>
             <h1>Rick and Motry</h1>
             <img src={imagen} />
         </div>
