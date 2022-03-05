fd6f84c970a12c6c327f5db7ae338cfb108fd4f8
German Koch <gjkoch7@gmail.com>
2022-03-05 15:02:10 -0300

    Se arreglo titulo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return(
         <div className="home">
             <h1>vamos boquita</h1>
-            <h1>Rick and Motry</h1>
+            <h1>Rick and Morty</h1>
             <img src={imagen} />
         </div>
     )
